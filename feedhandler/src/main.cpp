28815c0014054bc831d8785ed51a65a96d149013
Keerthivasan S V <cb.sc.u4aie23037@cb.students.amrita.edu>
2026-01-26 23:27:07 +0530

    week1 day2: update main.cpp to test TcpClient with interactive echo demo

--- feedhandler/src/main.cpp
+++ feedhandler/src/main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <string>
+#include "net/tcp_client.hpp"
 
 int main() {
-	std::cout << "FeedHandler Boot OK\n";
-	return 0;
+    std::cout << "FeedHandler Boot OK" << std::endl;
+    
+    feedhandler::net::TcpClient client;
+    
+    // Test connection to localhost:8080
+    if (client.connect("localhost", 8080)) {
+        std::cout << "Enter messages to send (type 'quit' to exit):" << std::endl;
+        
+        std::string input;
+        while (std::getline(std::cin, input) && input != "quit") {
+            if (client.send(input + "\n")) {
+                std::string response = client.recv();
+                if (!response.empty()) {
+                    std::cout << "Received: " << response;
+                }
+            }
+        }
+    } else {
+        std::cout << "Failed to connect. Make sure to run 'nc -l 8080' in another terminal." << std::endl;
+    }
+    
+    return 0;
 }
 
